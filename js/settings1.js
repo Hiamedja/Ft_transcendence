9cd75468d29272428fcb54d4f57d5f8695df5601
Hiba AMEDJAR <117330946+Hiamedja@users.noreply.github.com>
2025-05-14 13:09:42 +0100

    Update settings1.js

--- js/settings1.js
+++ js/settings1.js
@@ -1,15 +1,4 @@
 
-// async function showChangePassword() {
-//     document.getElementById('change-password-overlay').classList.remove('hidden');
-//     await check2FAStatus();
-    
-//   }
-
-// async function showChangePassword() {
-//   document.getElementById('change-password-overlay').classList.remove('hidden');
-//   await check2FAStatus('settings');
-// }
-
 
 async function showChangePassword() {
 
